123f329cb9772784da06c08688754e3dee667ba0
Zayeem Mohd <zayeemmohd@131-101-000-128.ip-addr.inexio.net>
2026-08-17 15:12:16 +0530

    first commit

---
+++ JAVASCRIPT/Day 1/script.js
@@ -0,0 +1,27 @@
+// let followers = 100;
+
+// followers = followers + 1
+// followers += 1
+// followers++
+// followers++
+// followers = followers + 1
+
+// followers = followers - 1
+
+// followers /= 10
+// followers = follwers / 10
+
+// console.log(followers)
+
+// let count = 5;
+
+// let result = count++;
+
+// console.log(result) // 5
+// console.log(count) // 6
+
+let age = 23;
+
+age += 2;
+
+console.log(age);
