229afcb3296d1410a87e081d7135c552bd4fdd2c
Zayeem Mohd <zayeemmohd@133-101-000-128.ip-addr.inexio.net>
2026-08-18 14:58:58 +0530

    js day 4 part-1

---
+++ JAVASCRIPT/Day 4/script.js
@@ -0,0 +1,118 @@
+// let str1 = "   Hyderbad   "
+
+// console.log(str1)
+// console.log(str1.trim());
+
+// const city = prompt("Enter you city: ");
+
+// let newCity = city.trim();
+
+// console.log(city)
+// console.log(newCity)
+
+// let city = "hyderabad";
+
+// const newCity = city.toUpperCase();
+
+// console.log(newCity);
+
+// let city = "HYDERABAD";
+
+// const newCity = city.toLowerCase();
+
+// console.log(newCity);
+
+// let str = "Javascript";
+
+// let foundIndex = str.indexOf("a");
+
+// console.log(foundIndex);
+
+// let message = "I love javascript";
+
+// let word = "Javascript";
+
+// const index = message.indexOf(word);
+
+// console.log(index);
+
+// let para =
+//   " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, amet laudantium sequi magnam iure voluptatem repellendus, dolorem voluptates expedita quos est porro maiores ad impedit pariatur eum modi eligendi non";
+
+// let userWord = prompt("Enter the word to find indexOf: ");
+
+// const indexResult = para.indexOf(userWord);
+
+// // console.log(indexResult)
+
+// if (indexResult === -1) {
+//   console.log("Word not found");
+// } else {
+//   console.log(`Word found at this index: ${indexResult}`);
+// }
+
+// let str1 = "    HYDERABAD   ";
+
+// let trimmedStr = str1.trim();
+
+// let finalStr = trimmedStr.toLowerCase();
+
+// console.log(finalStr);
+
+// let str1 = "    HYDERABAD   ";
+
+// const finalStr = str1.trim().toLowerCase();
+
+// console.log(finalStr)
+
+// console.log(str1)
+
+// const message = "Hello World";
+
+// let slicedWord = message.slice(0, 5);
+
+// console.log(slicedWord);
+
+// const word = "Javascript hello";
+
+// // const letter = word.slice(9, 10);
+
+// // const letter = word.slice(4);
+
+// const letter = word.slice(-1);
+
+// console.log(letter);
+
+
+// let message = "Hello, where are you?";
+
+// let result = "Hi, where are you?".replace("Hi", "Hello") ;
+
+// console.log(result)
+
+
+
+// let fruit1 = "green apple"
+
+// let fruit2 = "orange"
+
+// let fruit3 = "banana"
+
+// let fruit4 = "grapes"
+
+// let fruit5 = "dragon fruit"
+
+// console.log(fruit5)
+
+// CRUD operations: Create, Read, Update, Delete
+
+let fruits = ["green apple", "orange", "banana", "grapes", "dragon fruit", "kiwi"]
+
+const count = fruits.length;
+
+console.log(count)
+
+// Read Operation
+// let targetFruit = fruits[1]
+
+// console.log(targetFruit)
