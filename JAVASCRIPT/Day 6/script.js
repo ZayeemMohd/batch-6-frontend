da959a19d4518796fcfcea4473cdc9e5066c3adb
Zayeem Mohd <zayeemmohd@117-101-000-128.ip-addr.inexio.net>
2026-08-25 14:48:29 +0530

    Day 6 js math.random, and array of objects

--- JAVASCRIPT/Day 6/script.js
+++ JAVASCRIPT/Day 6/script.js
@@ -48,34 +48,42 @@
 
 // let caption = "It a sunny day"
 
-const post1 = {
-  caption: "It a sunny day",
-  username: "itz_crazy_xyz",
-  date: "12-08-2026",
-  imageURL:
-    "https://ik.imagekit.io/acrrubsd0/charminar.jpg?updatedAt=1784190842445",
-  likeCount: 2,
-  tags: ["#cool", "#super", "#crazy", "#hyderabad"],
-  comments: {
-    hidden_123: "wow, nice photo",
-    khaja_26: "nice, photo",
-  },
-};
+// const post1 = {
+//   caption: "It a sunny day",
+//   username: "itz_crazy_xyz",
+//   date: "12-08-2026",
+//   imageURL:
+//     "https://ik.imagekit.io/acrrubsd0/charminar.jpg?updatedAt=1784190842445",
+//   likeCount: 2,
+//   tags: ["#cool", "#super", "#crazy", "#hyderabad"],
+
+// };
+
+// post1.comments = {
+//     comment1: {
+//       username: "Rehman",
+//       comment: "Nice photo",
+//     },
+//     comment2: {
+//       username: "khaja_26",
+//       comment: "nice, photo",
+//     },
+//   },
 
 // add
-post1.city = "hyderabad";
+// post1.city = "hyderabad";
 
 //update value
-post1.username = "itz_crazy_abc";
-console.log(post1);
+// post1.username = "itz_crazy_abc";
+// console.log(post1);
 
 // update value
-post1.likeCount = post1.likeCount + 1;
+// post1.likeCount = post1.likeCount + 1;
 
 //delete
 
-delete post1.date
-console.log(post1)
+// delete post1.date
+// console.log(post1)
 
 // console.log(post1.comments.hidden_123);
 // console.log(post1.comments.khaja_26);
@@ -85,3 +93,153 @@ console.log(post1)
 // console.log(post1.tags[1]);
 // console.log(post1.tags[2]);
 // console.log(post1.tags[3]);
+
+// let user1 = {
+//   // key: value
+//   firstLast: "Mohd",
+//   "lastName": "Khan",
+//   "@location": "abcd, hyderabad"
+// }
+
+// console.log(user1["@location"])
+
+// variables
+// let myKey =  "lastName"
+
+// console.log(user1[myKey])
+// let fruits = ["orange", "apple", "grapes", "mango"];
+
+// let book = {
+//   title: "JavaScript Guide",
+//   author: "John Doe",
+//   pages: 350,
+//   published: 2024,
+// };
+
+// update a key value pair in existing object
+// book.pages = 450;
+
+// add a key value pair in existing object
+// book.price = 1000;
+
+// let userKey = prompt("Enter the key you want value of: ");
+
+// console.log(book[userKey]);
+// book.userkey
+
+// let user = {};
+
+// user.name = prompt("Enter your name");
+// user.age = prompt("Enter your age");
+// user.address = prompt("Enter your address");
+
+// console.log(`These are you details: `, user);
+
+// let users = ["", "", true, 333, [], {}]
+
+// let student1 = {
+//   name: "Khaled",
+//   age: 20,
+//   address: "Charminar"
+// }
+
+// let student2 = {
+//   name: "Qhadeer",
+//   age: 21,
+//   address: "Malakpet"
+// }
+
+// let student3 = {
+//   name: "Asif",
+//   age: 21,
+//   address: "AkbarBagh"
+// }
+
+// let users = [
+//   {
+//     name: "Khaled",
+//     age: 20,
+//     address: "Charminar",
+//   },
+//   {
+//     name: "Asif",
+//     age: 21,
+//     address: "AkbarBagh",
+//   },
+//   {
+//     name: "Asif",
+//     age: 21,
+//     address: "AkbarBagh",
+//   },
+// ];
+
+// Options
+// 1. Add a user
+// 2. Print all users
+// 3. Exit
+
+// while (true) {
+//   let userOption = prompt(
+//     "Options: \n 1. Add a user \n 2. Print all users  \n 3. Exit \n Choose you option:: ",
+//   );
+
+//   if (userOption == "1") {
+//     let name = prompt("Enter your name: ");
+//     let age = prompt("Enter your age: ");
+//     let address = prompt("Enter your address: ");
+
+//     let userObj = {
+//       name: name,
+//       age: age,
+//       address: address,
+//     };
+
+//     users.push(userObj);
+//     console.log("user added successfully");
+//   } else if (userOption == "2") {
+//     console.log(users);
+//   } else if (userOption == "3") {
+//     break;
+//   } else {
+//     console.log("You chose wrong options");
+//   }
+// }
+
+// let name = prompt("Enter your name: ");
+// let age = prompt("Enter your age: ")
+// let address = prompt("Enter your address: ")
+
+// let userObj = {
+//   "name": name,
+//   "age": age,
+//   "address": address
+// }
+
+// users.push(userObj)
+
+// let users = {
+//   student1: {
+
+//   },
+//   student2: {
+
+//   },
+//   student3:  {
+
+//   }
+// }
+
+
+// let Math = {
+//   // property: key value pair
+//   PI: 3.14, 
+//   g: 9.8,
+   
+// }
+
+
+
+// console.log(Math.PI)
+
+
+
---
+++ JAVASCRIPT/Day 6/script2.js
@@ -0,0 +1,31 @@
+// Math.round(4.7)
+// 5
+// Math.round(4.2)
+// 4
+// Math.round(4.5)
+// 5
+// Math.floor(4.5)
+// 4
+// Math.floor(4.9)
+
+// console.log(Math.floor(4.7)); // 4
+
+// console.log(Math.ceil(4.1));
+
+// Math.random() random value b/w 0-1 1 is excluded
+
+// let randomVal = Math.random() * 1000 // 0-999
+// let randomVal = Math.floor( Math.random() * 10 ) + 1 // 0-9 // 1-10
+
+// console.log(randomVal)
+
+// Math.floor(Math.random() * (max - min + 1) + min)
+
+// 5 -10
+// let random = Math.floor(Math.random() * (10 - 5 + 1) + 5)
+// console.log(random);
+
+// let dice = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+
+// console.log(dice);
+
