fdce1596fc9cfcb537c0565acea1b1145dc82952
Zayeem Mohd <zayeemmohd@116-101-000-128.ip-addr.inexio.net>
2026-08-28 14:14:40 +0530

    day 7 part-1

---
+++ JAVASCRIPT/Day 7/script.js
@@ -0,0 +1,83 @@
+// step 1: creating a function
+// function definition
+// parameter
+// function printIntro(username, city) {
+
+//     // let city = "Hyderabad"
+
+//   console.log("Hello");
+//   console.log(`I am ${username}`);
+//   console.log(`I live in ${city}`);
+//   console.log("Bye");
+// }
+
+// // step 2: calling a function
+
+// // arguments
+// printIntro("Abdul Rahman", "Hyderabad");
+// printIntro("Abdul Raheem", "Mumbai");
+// printIntro("Abdul Kareem", "Delhi")
+
+// printIntro("Abdul Raheem");
+
+// printIntro("Abdul Kareem");
+
+// repeatation
+// DRY (Don't repeat yourself)
+
+// print no b/w 1-6
+// function rollDice() {
+//   let result = Math.floor(Math.random() * 6 + 1);
+//   console.log(result);
+// }
+
+// let result1 = Math.floor(Math.random() * 6 + 1);
+// console.log(result1);
+
+// let result2 = Math.floor(Math.random() * 6 + 1);
+// console.log(result2);
+
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+// rollDice();
+
+// parameter
+// function printMessage(username, age) {
+//   console.log(`My name is ${username}, and I am ${age} years old.`);
+// }
+
+// printMessage("Abdul rahman", 22);
+
+// printMessage("Abdul raheem", 52);
+
+// printMessage("Abdul Kareem", 67);
+
+// .splice(2, 1, "apple")
+
+// .slice(2, 3)
+
+// function sum(num1, num2) {
+//   let result = num1 + num2;
+//   return result
+// }
+
+// let returnValue = sum(2, 2);
+
+
+
+// console.log(result)
+
+// function mul(num1, num2) {
+//   let result = num1 * num2;
+//   return result
+// }
+
+// sum(2, 5);
+// mul(2, 4);
+
+// let result = 22;
+
+// let result = 34;
