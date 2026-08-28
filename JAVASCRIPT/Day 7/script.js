9468a80df87f32a03c804fb0f010ea2890132d66
Zayeem Mohd <zayeemmohd@116-101-000-128.ip-addr.inexio.net>
2026-08-28 15:05:51 +0530

    day 7 js part 2

--- JAVASCRIPT/Day 7/script.js
+++ JAVASCRIPT/Day 7/script.js
@@ -66,8 +66,6 @@
 
 // let returnValue = sum(2, 2);
 
-
-
 // console.log(result)
 
 // function mul(num1, num2) {
@@ -81,3 +79,91 @@
 // let result = 22;
 
 // let result = 34;
+
+// 1. Input? -
+// 2. Logic? -
+// 3. Output? -
+
+// function definition
+// function getAverage(num1, num2, num3) {
+//   let avg = (num1 + num2 + num3) / 3;
+
+//   return avg;
+// }
+
+// // function calling
+// const result = getAverage(20, 20, 20);
+// console.log(result);
+
+// function printAverage(){
+
+// }
+
+// Global scope
+
+// function div(a, b){
+//   // function scope
+//   let username = "Khalid"
+// }
+
+// function sum(a, b){
+//   console.log(username)
+//   let result = a + b
+//   return result
+// }
+
+// sum(1,3)
+
+// console.log(result)
+
+// function mul(a, b){
+//   let result = a * b
+
+//   return result
+// }
+
+// let result = 323
+// let result = 222
+
+// for(let i = 0; i<10; i++){
+
+// }
+
+// console.log(i)
+
+// {
+//   // block scope
+//   const age = 22;
+// }
+
+// console.log(age);
+
+// function parentFn() {
+//   let x = 22;
+
+//   function childFn() {
+//     console.log("x value in child fn: ",x);
+
+//     function grandChildFn(){
+//       console.log("x value in grand child fn", x)
+//     }
+
+//     grandChildFn()
+
+//   }
+
+//   childFn();
+// }
+
+// parentFn();
+
+// function sum() {}
+
+// // function expression
+// let firstVar = function (a, b) {
+//   console.log("hello");
+// };
+
+// // console.log(firstVar);
+
+// firstVar();
