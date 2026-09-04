6fbcd4ca14222098d0d1b953071a93e5c481796a
Zayeem Mohd <zayeemmohd@136-101-000-128.ip-addr.inexio.net>
2026-09-04 15:05:15 +0530

    complete day 10

---
+++ JAVASCRIPT/Day 10-2/script.js
@@ -0,0 +1,24 @@
+// console.log("hello world");
+
+// const heading1 = document.querySelector("#heading1");
+
+// console.log(heading1.innerText);
+
+// const overviewText = document.querySelector("#overview-text");
+
+// console.log(overviewText.innerText);
+// console.log(overviewText.textContent);
+// console.log(overviewText.innerHTML);
+
+// 1 selecting an element
+
+// const heading1 = document.querySelector("#heading1");
+
+// heading1.innerText = "Our Amazing Solar System";
+
+// const imgArr = document.querySelectorAll(".oldImg");
+// console.log(imgArr.length);
+
+// const boxDiv = document.querySelector(".box div")
+
+
--- JAVASCRIPT/Day 10/script.js
+++ JAVASCRIPT/Day 10/script.js
@@ -13,10 +13,50 @@ console.dir(document);
 // };
 
 // reading an object
-console.log(document.all[6].innerText);
+// console.log(document.all[6].innerText);
 
-// updating an object value
+// // updating an object value
 
-document.all[6].innerText = "Hi you are hacked";
+// document.all[6].innerText = "Hi you are hacked";
 
-console.log(document);
+// console.log(document);
+
+// 1: selecting an HTML element
+
+// console.log()
+
+// setTimeout(() => {
+//   console.log("Testing timeout");
+//   document.all[6].innerText = "Hi How are you";
+// }, 2000);
+
+// methods
+// 1 select elements by tag name
+
+// document.all[6].innerText
+
+// array, collection, nodeList []
+
+// return an array
+// const h1Arr = document.getElementsByTagName("h1");
+
+// h1Arr[1].innerText = "Abc"
+
+// 2. select elements by id
+
+// const mainH1 = document.getElementById("main-heading");
+
+// console.log(mainH1);
+
+// mainH1.innerText = "Abc";
+
+// 3. select element by classname
+
+// const liArr = document.getElementsByClassName("list-item");
+
+// console.log(liArr);
+
+// liArr[2].innerText = liArr[2].innerText + " on time";
+
+const liArr = document.querySelectorAll(".list-item");
+console.log(liArr);
